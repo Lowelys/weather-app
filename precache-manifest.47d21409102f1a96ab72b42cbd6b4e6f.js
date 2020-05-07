258cda74855790795aa35e168f99be49a427171a
Sergey Belov <48603698+Lowelys@users.noreply.github.com>
2020-05-07 15:08:36 +0300

    Updates

---
+++ precache-manifest.47d21409102f1a96ab72b42cbd6b4e6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d40bcb14247dfa17f89379ad47b5d6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4296a9b05f75d13895fd",
+    "url": "/weather-app/static/css/main.fa6415f8.chunk.css"
+  },
+  {
+    "revision": "23def431e6424c576d41",
+    "url": "/weather-app/static/js/2.d74e9ce8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.d74e9ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4296a9b05f75d13895fd",
+    "url": "/weather-app/static/js/main.4b192ef1.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  },
+  {
+    "revision": "e7e5a5b496d2b405da2833340ce1067c",
+    "url": "/weather-app/static/media/weather.e7e5a5b4.jpg"
+  }
+]);
